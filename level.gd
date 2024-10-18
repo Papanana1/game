66a92a5df97c54ecf8525dc20af68edbb0238e48
user4696 <bergerong@sbschools.net>
2024-10-18 16:52:36 -0400

    HUD

--- level.gd
+++ level.gd
@@ -11,9 +11,5 @@ func _process(delta):
 	pass
 
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://level.tscn")
-
-
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_level_one_pressed():
+	get_tree().change_scene_to_file("res://level_one.tscn")
